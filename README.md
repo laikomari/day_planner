874a69fc37b00e37d17af05952d01ae9c2732f33
laikomari <laikocayanan@gmail.com>
2022-08-20 17:47:01 -0500

    day planner files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # day_planner
+
+https://github.com/laikomari/day_planner.git
---
+++ script.js
@@ -0,0 +1,35 @@
+$(init);
+
+function init() {
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+  colorTimeBlocks();
+  setInterval(colorTimeBlocks, 60000);
+
+  $(".time-block").each(function() {
+    var blockId = $(this).attr("id");
+    $("#" + blockId + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
+  });
+
+  $(".saveBtn").on("click", handleSave);
+}
+
+function colorTimeBlocks() {
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").replace("hour-", ""));
+    var currentHour = parseInt(moment().format("H"));
+    $(this).removeClass("past present future");
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour > currentHour) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+}
+
+function handleSave(event) {
+  var hourId = $(this).parent().attr("id");
+  localStorage.setItem(moment().format("DDDYYYY") + hourId, $("#" + hourId + " textarea").val());
+}
